b0893d6787e0be528c29b68c447150e16b0db238
Michael Landis <michael@momentohq.com>
2024-10-21 12:24:20 -0700

    feat: support zrem command
    Adds the `zrem` command, integration tests, and interface method.

--- src/IMomentoRedisClient.php
+++ src/IMomentoRedisClient.php
@@ -14,6 +14,7 @@ interface IMomentoRedisClient
     public function setnx(string $key, mixed $value): Redis|bool;
     public function ttl(string $key): Redis|int|false;
     public function zAdd(string $key, float|array $score_or_options, mixed ...$more_scores_and_mems): Redis|int|float|false;
+    public function zRem(mixed $key, mixed $member, mixed ...$other_members): Redis|int|false;
     public function zRevRange(string $key, int $start, int $end, mixed $scores = null): Redis|array|false;
     public function zScore(string $key, mixed $member): Redis|float|false;
 }
