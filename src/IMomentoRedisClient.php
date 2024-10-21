6c135c27ab428228b17945520b9286dc9e6a0390
Michael Landis <malandis@gmail.com>
2024-10-21 10:15:00 -0700

    chore: backfill interface methods (#10)
    Since we have implemented various new mehtods in the client, we
    backfill the interface with the new additions.

--- src/IMomentoRedisClient.php
+++ src/IMomentoRedisClient.php
@@ -2,8 +2,17 @@
 
 namespace Momento\Cache;
 
+use Redis;
+
 interface IMomentoRedisClient
 {
-    public function set(string $key, string $value, mixed $options = null): mixed;
+    public function del(array|string $key, string ...$other_keys): Redis|int|false;
+    public function expire(string $key, int $timeout, ?string $mode = null): Redis|bool;
     public function get(string $key): mixed;
+    public function incrBy(string $key, int $value): Redis|int|false;
+    public function set(string $key, string $value, mixed $options = null): mixed;
+    public function setnx(string $key, mixed $value): Redis|bool;
+    public function ttl(string $key): Redis|int|false;
+    public function zAdd(string $key, float|array $score_or_options, mixed ...$more_scores_and_mems): Redis|int|float|false;
+    public function zRevRange(string $key, int $start, int $end, mixed $scores = null): Redis|array|false;
 }
