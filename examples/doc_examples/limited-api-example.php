850a075336f157ec57d2ccceadc71a63ca91e4aa
rishtigupta <rishti@momentohq.com>
2024-10-23 12:35:52 -0700

    chore: remove main function from the doc examples

--- examples/doc_examples/limited-api-example.php
+++ examples/doc_examples/limited-api-example.php
@@ -1,34 +1,24 @@
 <?php
 declare(strict_types=1);
 
-
 use Momento\Auth\CredentialProvider;
 use Momento\Cache\CacheClient;
-use Momento\Cache\Errors\InvalidArgumentError;
 use Momento\Cache\MomentoCacheClient;
 use Momento\Config\Configurations\Laptop;
 use Momento\Logging\StderrLoggerFactory;
 
 require "vendor/autoload.php";
 
-// Setup global variables for cache name, TTL, key, and value
 $CACHE_NAME = uniqid("php-example-");
 $ITEM_DEFAULT_TTL_SECONDS = 60;
 $KEY = uniqid("myKey-");
 $VALUE = uniqid("myValue-");
 
-// Setup MomentoCacheClient
-/**
- * @throws InvalidArgumentError | Exception
- */
-function main(): void
-{
-    // Create a Momento cache client
-    $authProvider = CredentialProvider::fromEnvironmentVariable("MOMENTO_API_KEY");
-    $configuration = Laptop::latest(new StderrLoggerFactory());
-    $client = new CacheClient($configuration, $authProvider, $GLOBALS['ITEM_DEFAULT_TTL_SECONDS']);
-    $logger = $configuration->getLoggerFactory()->getLogger("ex:");
+// Create a Momento cache client
+$authProvider = CredentialProvider::fromEnvironmentVariable("MOMENTO_API_KEY");
+$configuration = Laptop::latest(new StderrLoggerFactory());
+$client = new CacheClient($configuration, $authProvider, $ITEM_DEFAULT_TTL_SECONDS);
+$logger = $configuration->getLoggerFactory()->getLogger("ex:");
 
-    // This interface provides type safety as it only allows the user to interact with the Redis commands that are supported by MomentoCacheClient class
-    $momentoCacheClient = new MomentoCacheClient($client, $GLOBALS['CACHE_NAME']);
-}
+// This interface provides type safety as it only allows the user to interact with the Redis commands that are supported by MomentoCacheClient class
+$momentoCacheClient = new MomentoCacheClient($client, $CACHE_NAME);
--- examples/doc_examples/readme-example.php
+++ examples/doc_examples/readme-example.php
@@ -1,41 +1,31 @@
 <?php
 declare(strict_types=1);
 
-
 use Momento\Auth\CredentialProvider;
 use Momento\Cache\CacheClient;
-use Momento\Cache\Errors\InvalidArgumentError;
 use Momento\Cache\MomentoCacheClient;
 use Momento\Config\Configurations\Laptop;
 use Momento\Logging\StderrLoggerFactory;
 
 require "vendor/autoload.php";
 
-// Setup global variables for cache name, TTL, key, and value
 $CACHE_NAME = uniqid("php-example-");
 $ITEM_DEFAULT_TTL_SECONDS = 60;
 $KEY = uniqid("myKey-");
 $VALUE = uniqid("myValue-");
 
-// Setup MomentoCacheClient
-/**
- * @throws InvalidArgumentError | Exception
- */
-function main(): void
-{
-    // Create a Momento cache client
-    $authProvider = CredentialProvider::fromEnvironmentVariable("MOMENTO_API_KEY");
-    $configuration = Laptop::latest(new StderrLoggerFactory());
-    $client = new CacheClient($configuration, $authProvider, $GLOBALS['ITEM_DEFAULT_TTL_SECONDS']);
-    $logger = $configuration->getLoggerFactory()->getLogger("ex:");
+// Create a Momento cache client
+$authProvider = CredentialProvider::fromEnvironmentVariable("MOMENTO_API_KEY");
+$configuration = Laptop::latest(new StderrLoggerFactory());
+$client = new CacheClient($configuration, $authProvider, $ITEM_DEFAULT_TTL_SECONDS);
+$logger = $configuration->getLoggerFactory()->getLogger("ex:");
 
-    // Create a Redis client backed by Momento cache client over the cache
-    $momentoCacheClient = new MomentoCacheClient($client, $GLOBALS['CACHE_NAME']);
+// Create a Redis client backed by Momento cache client over the cache
+$momentoCacheClient = new MomentoCacheClient($client, $CACHE_NAME);
 
-    // Perform operations vs Momento as if using a regular Redis client
-    $setResult = $momentoCacheClient->set($GLOBALS['KEY'], $GLOBALS['VALUE']);
-    $logger->info("Set result: " . $setResult . "\n");
+// Perform operations vs Momento as if using a regular Redis client
+$setResult = $momentoCacheClient->set($KEY, $VALUE);
+$logger->info("Set result: " . $setResult . "\n");
 
-    $getResult = $momentoCacheClient->get($GLOBALS['KEY']);
-    $logger->info("Get result: " . $getResult . "\n");
-}
+$getResult = $momentoCacheClient->get($KEY);
+$logger->info("Get result: " . $getResult . "\n");
